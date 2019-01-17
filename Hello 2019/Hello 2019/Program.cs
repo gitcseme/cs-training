b59cfdd33693bcd83d9c23617dfc68d105de8c07
gitcseme <shuvo_CSE23@yahoo.com>
2019-01-17 12:36:37 +0600

    first commit with .gitignore

---
+++ Hello 2019/Hello 2019/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello cruel world.");
+        }
+    }
+}
