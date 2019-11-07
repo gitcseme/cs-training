79a79f63fc9e94170827e5a55e3479d3ed9df762
kashuvo <shuvo_CSE23@yahoo.com>
2019-11-08 02:11:06 +0600

    Builder pattern using htmlbuilder

---
+++ Design Pattern Tutorial/BuilderPattern/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPattern
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public int IndentSize = 4;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+
+        public HtmlElement()
+        {
+
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImp(int indent)
+        {
+            var sb = new StringBuilder();
+            string i = new string(' ', IndentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', IndentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var el in Elements)
+            {
+                sb.Append(el.ToStringImp(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImp(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string RootName;
+        HtmlElement root = new HtmlElement();
+        
+        public HtmlBuilder(string rootName)
+        {
+            RootName = rootName;
+            root.Name = rootName;
+        }
+
+        public void AddChild(string childName, string childText)
+        {
+            HtmlElement el = new HtmlElement(childName, childText);
+            root.Elements.Add(el);
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = RootName};
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb.ToString());
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.Append("</ul>");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("\n");
+
+
+            HtmlBuilder builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "Hello");
+            builder.AddChild("li", "World");
+            Console.WriteLine(builder.ToString());
+            builder.Clear();
+        }
+    }
+}
